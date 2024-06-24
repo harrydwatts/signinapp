0dff10157a42b7d8b120e88d9ec9984301479e20
Harry Watts <h.watts@sec-storage.co.uk>
2024-06-24 08:50:09 +0100

    Test 8

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,7 +1,11 @@
 import axios from 'axios';
 
+const baseURL = process.env.VUE_APP_API_BASE_URL || '/api';
+
+console.log(`Axios base URL: ${baseURL}`); // Log the base URL being used
+
 const instance = axios.create({
-  baseURL: process.env.VUE_APP_API_BASE_URL || '/api',
+  baseURL: baseURL,
   headers: {
     'Content-Type': 'application/json',
   },
